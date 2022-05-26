c4041d295b26e291e1e3366996812f2da08197cc
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-05-26 20:44:06 +0300

    update(pub): now pub validate data

--- pub/main.go
+++ pub/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/nats-io/stan.go"
@@ -13,21 +16,13 @@ import (
 	"github.com/yalagtyarzh/L0/pub/internal/config"
 )
 
+var dir = "./pub/orders"
+var validExt = ".json"
+
+// main simulates sending data to channel via api
 func main() {
 	cfg := config.GetConfig()
-	var orders []models.Order
-	file, err := os.Open("./pub/internal/config/orders.json")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer file.Close()
-
-	data, err := io.ReadAll(file)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	err = json.Unmarshal(data, &orders)
+	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -35,18 +30,57 @@ func main() {
 	sc, _ := stan.Connect(cfg.STAN.Cluster, cfg.STAN.Client)
 	defer sc.Close()
 
-	for _, order := range orders {
+	for _, file := range files {
+		fileName := file.Name()
+		if !checkExt(fileName, validExt) {
+			log.Println("Invalid file extension of file", fileName)
+			continue
+		}
+
+		f, err := os.Open(fmt.Sprintf("%s/%s", dir, fileName))
+		if err != nil {
+			fmt.Printf("Couldn't open file: %s\n", err)
+			continue
+		}
+		defer f.Close()
+
+		data, err := io.ReadAll(f)
+		if err != nil {
+			log.Printf("Couldn't read file: %s\n", err)
+			continue
+		}
+
+		var order models.Order
+		err = json.Unmarshal(data, &order)
+		if err != nil {
+			log.Printf("Invalid json: %s\n", err)
+			continue
+		}
+
 		jsonData, err := json.Marshal(order)
 		if err != nil {
-			log.Fatalln(err)
+			log.Printf("Couldn't marshal: %s\n", err)
+			continue
 		}
 
 		err = sc.Publish("foo", jsonData)
-		log.Println("sent")
 
 		if err != nil {
 			log.Printf("error to send data: %s", err)
+			continue
 		}
+
+		log.Println("sent")
 		time.Sleep(time.Second * time.Duration(cfg.STAN.Delay))
 	}
+}
+
+// checkExt checks for valid file extension
+func checkExt(filename string, ext string) bool {
+	fileExt := filepath.Ext(filename)
+	if fileExt != ext {
+		return false
+	}
+
+	return true
 }
